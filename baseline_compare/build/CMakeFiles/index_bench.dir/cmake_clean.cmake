d6b57ed3c302d0e6e7225610a9c944a60aa351cd
dong <90365351+CHAN-dong@users.noreply.github.com>
2026-04-08 20:27:54 +1000

    Add files via upload

---
+++ baseline_compare/build/CMakeFiles/index_bench.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/index_bench.dir/src/unorder_main.cpp.o"
+  "CMakeFiles/index_bench.dir/src/unorder_main.cpp.o.d"
+  "index_bench"
+  "index_bench.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/index_bench.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
